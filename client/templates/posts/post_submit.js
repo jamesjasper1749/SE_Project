8ab9859e7d302e2a0776ea89654f4ed6f489ce51
Paphan <jamesgames9999@hotmail.com>
2017-08-06 23:40:17 +0700

    Clear errors after 3 seconds.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,12 +11,14 @@ Template.postSubmit.events({
     Meteor.call('postInsert', post, function(error, result) {      
     
     // display the error to the user and abort      
-    if (error)        
-    	return alert(error.reason);      
+    if (error)
+      return throwError(error.reason);        
+    	// return alert(error.reason);      
     
 	// show this result but route anyway      
-	if (result.postExists)        
-		alert('This link has already been posted');
+	if (result.postExists)     
+    throwError('This link has already been posted');   
+		// alert('This link has already been posted');
 
     // Router.go('postPage', {_id: result._id});      
 	});
